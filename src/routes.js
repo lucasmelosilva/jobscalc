8a8fd592b988bb586ebf4812e8f3cb81fc939a7c
Lucas Melo <lucas.melo_7@hotmail.com>
2021-06-03 16:00:09 -0300

    fix dos paths && href

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routes = express.Router();
+const basePath = __dirname + '/views/';
 
-routes.get('/', (request, response) => {
+routes.get('/', (req, res) => res.sendFile(basePath + '/index.html'))
+routes.get('/job', (req, res) => res.sendFile(basePath + '/job.html'))
+routes.get('/job/edit', (req, res) => res.sendFile(basePath + '/job-edit.html'))
+routes.get('/profile', (req, res) => res.sendFile(basePath + '/profile.html'))
 
-    return response.sendFile(__dirname + '/views/index.html')
-
-})
 
 
 module.exports = routes
