8551d7809a5c0f5a7ca3c3b252f797253d16a4e2
Lucas Melo <lucas.melo_7@hotmail.com>
2021-06-03 15:23:06 -0300

    Primeira rota criada. Rota para o index

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 const server = express()
 
-server.get('/', (request, response) => {
-
-    return response.send('oiiii')
-})
-
+// Habilitar arquivos static
+server.use(express.static('public'))
+server.use(routes)
 server.listen(3000, () => {
     console.log('Oi :D')
 })
