fa0babff2354a75f3f771dba26dce7c69ffb193d
lucas melo <lucas.melo044@gmail.com>
2021-06-16 18:24:16 -0300

    Refatoracao do profile && calculo do valor da hora

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,31 @@ const Profile = {
         },
 
         update(req, res) {
-            
+            // req.body para pegar os dados
+            const data = req.body
+
+            // definir quantas semanas tem um ano: 52
+            const weeksPerYear = 52
+
+            // remover as semanas de ferias do ano, para pegar quantas semanas tem um 1 mes
+            const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+
+            // total de horas trabalhadas na semana
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+            // horas trabalhadas no mes
+            const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+            // qual sera o valor da minha hora?
+            const valueHour = data["monthly-budget"] / monthlyTotalHours
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                "value-hour": valueHour
+            }
+
+            return res.redirect('/profile')
         }
     }
 }
