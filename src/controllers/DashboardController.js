9d9933fb21847c968c09d0e184d10f50fa303606
lucas melo <lucas.melo044@gmail.com>
2021-06-21 23:43:16 -0300

    Add calculo para saber o total, a quantidade de done e progress dos jobs

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -4,19 +4,34 @@ const Profile = require('../models/Profile')
 
 module.exports = {
   index(req, res) {
+    const profile = Profile.get();
     const jobs = Job.get()
+
+    const statusCount = {
+      progress: 0,
+      done: 0,
+      total: jobs.length
+    }
+
     const updateJobs = jobs.map(job => {
       const remaining = JobUtils.remainingDays(job)
       const status = remaining <= 0 ? "done" : "progress"
+
+      /* 
+        status = 'done'
+        statusCount[status] === statusCount[done] === statusCount.done += 1
+        somando mais 1 a quantidade de status
+      */
+      statusCount[status] += 1;
 
       return {
         ...job,
         remaining,
         status,
-        budget: JobUtils.calculateBudget(job, Profile.get()["value-hour"])
+        budget: JobUtils.calculateBudget(job, profile["value-hour"])
       }
     })
 
-    return res.render('index', { jobs: updateJobs })
+    return res.render('index', { jobs: updateJobs, profile, statusCount })
   }
 }
